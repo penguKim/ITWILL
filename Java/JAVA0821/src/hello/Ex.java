949af29463781105a9c739bb1a5933312a1fb696
penguKim <kpg8226@gmail.com>
2023-09-09 23:34:16 +0900

    Java 교육 시작

---
+++ Java/JAVA0821/src/hello/Ex.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Ex {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("이재환");
+		System.out.println(".");
+		System.out.println("");
+	}
+  
+}
