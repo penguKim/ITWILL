6828254b6cdc5f1048190ca0953a3534e93a5466
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-09-11 09:53:35 +0900

    주석 달기

---
+++ academy_Java/JAVA0822/src/comment/Ex.java
@@ -0,0 +1,31 @@
+package comment;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("10");
+		
+		/*이곳은 범위 주석 공간으로
+		*여러줄에 걸친 프로그램 설명을 작성하는 용도로 사용
+		*
+		*/
+		
+		/** 이 곳은 javaDoc 주석 공간으로
+		 * 자바 클래스 설명서를 제작하는 용도의 주석입니다.
+		 */
+	}	
+		
+		
+	
+
+}
