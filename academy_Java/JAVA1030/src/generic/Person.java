5f8cc1933fb6e585d4524adecab60f1dc22f9e72
penguKim <kpg8226@gmail.com>
2023-10-30 20:55:30 +0900

    제네릭

---
+++ academy_Java/JAVA1030/src/generic/Person.java
@@ -0,0 +1,21 @@
+package generic;
+
+public class Person { // Person extends Object
+	String name;
+	int age;
+	
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+	
+	
+}
